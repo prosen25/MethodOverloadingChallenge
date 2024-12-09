46ad546f0e9bf83dda4ec6878f864317cdf09553
Prosenjit Mondal <contact.prosen25@gmail.com>
2024-12-09 13:52:26 +0000

    Covert feet and inches to centimeters

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,16 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Height in centimeters = " + convertToCentimeters(5, 8));
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public static double convertToCentimeters (int heightInInches) {
+
+        return heightInInches * 2.54;
+    }
+
+    public static double convertToCentimeters (int heightInFeet, int heightInInches) {
+
+        return convertToCentimeters((heightInFeet * 12) + heightInInches);
     }
 }
